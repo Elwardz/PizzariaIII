5d5efa0220ac24900c5c32b373420c1505d058eb
Gabriel Lopes <gabriel.lopes.albuquerque@gmail.com>
2024-10-15 20:06:46 -0300

    Updaate em Classe Base Pizza e aplicando o Composite

--- src/application/ProgramPizza.java
+++ src/application/ProgramPizza.java
@@ -1,36 +1,3 @@
 package application;
 
-import entities.AtumPizza;
-import entities.ChickenPizza;
-import composite.MenuComponent;
-import composite.PizzaGroup;
-import static java.awt.SystemColor.menu;
 
-public class ProgramPizza {
-
-    public static void main(String[] args) {
-        // Criando pizzas a minha pizza individual
-        MenuComponent atumPizza = new AtumPizza();
-        MenuComponent chickenPizza = new ChickenPizza();
-
-        // Criando Menu de pizzas salgadas
-        PizzaGroup saltyPizzas = new PizzaGroup("Salty Pizzas");
-        saltyPizzas.add(atumPizza);
-        saltyPizzas.add(chickenPizza);
-
-        // Criando Menu de "Pizzas da Casa"
-        PizzaGroup housePizzas = new PizzaGroup("House Pizzas");
-        housePizzas.add(new AtumPizza()); // Pode adicionar mais pizzas aqui
-
-        // Grupo principal de pizzas
-        PizzaGroup allPizzas = new PizzaGroup("All Pizzas");
-        allPizzas.add(saltyPizzas);
-        allPizzas.add(housePizzas);
-
-        // Imprimindo o menu
-        System.out.println(allPizzas.getDescription());
-
-        // Custo total
-        System.out.println("Total cost: $" + allPizzas.cost()); //
-    }
-}
